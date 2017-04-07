42a7f992797c3211d4d7e1cc5920aaf6addd19b5
Exequiel Ceasar Navarrete <esnavarrete1@up.edu.ph>
2017-04-07 18:48:16 +0800

    Create blank earth engine sub-application

---
+++ earth_engine/models.py
@@ -0,0 +1,12 @@
+# models.py
+#
+# Copyright(c) Exequiel Ceasar Navarrete <esnavarrete1@up.edu.ph>
+# Licensed under MIT
+# Version 0.0.0
+
+from __future__ import unicode_literals
+from django.db import models
+
+# Create your models here.
+
+
---
+++ earth_engine/tests.py
@@ -0,0 +1,12 @@
+# tests.py
+#
+# Copyright(c) Exequiel Ceasar Navarrete <esnavarrete1@up.edu.ph>
+# Licensed under MIT
+# Version 0.0.0
+
+from __future__ import unicode_literals
+from django.test import TestCase
+
+# Create your tests here.
+
+
